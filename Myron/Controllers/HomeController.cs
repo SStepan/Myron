29b04644727cfa0f186635bf7f9df14496d328be
Stepan Myroniuk <smyroniuk@sharpminds.com>
2017-08-10 20:40:09 +0300

    Second commit

--- Myron/Controllers/HomeController.cs
+++ Myron/Controllers/HomeController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Myron.Models;
+using Myron.Services;
 
 namespace Myron.Controllers {
 	public class HomeController : Controller {
+		private IMailService _mail;
+
+		public HomeController(IMailService mail) {
+			_mail = mail;
+		}
+
 		public ActionResult Index() {
 			return View();
 		}
@@ -18,6 +26,17 @@ namespace Myron.Controllers {
 
 		public ActionResult Contact() {
 			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Contact(ContactModel model) {
+			var msg = $"Comment from: {model.Name} \nEmail: {model.Email}\nWebsite: {model.Website}\nComment: {model.Comment}"; 
+
+			if (_mail.SendEmail("myron@gmail.com", model.Email, "Myron's blog", msg)) {
+				ViewBag.MailSent = true;
+			}
 
 			return View();
 		}
---
+++ Myron/Services/MailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Myron.Services {
+	public class MailService : IMailService {
+		public bool SendEmail(string from, string to, string subject, string body) {
+			return true;
+		}
+	}
+}
