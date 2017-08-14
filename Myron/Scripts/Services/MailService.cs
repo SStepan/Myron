d72f1d2c50db915dfff17972811cc00db89b06fb
Stepan Myroniuk <myronyuk.s@gmail.com>
2017-08-14 21:58:51 +0300

    return files

---
+++ Myron/Scripts/Services/MailService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Myron.Services {
+	public class MailService : IMailService {
+		public bool SendEmail(string from, string to, string subject, string body) {
+			try {
+				var msg = new MailMessage(from, to, subject, body);
+
+				var smtp = new SmtpClient();
+				smtp.Send(msg);
+			} catch (Exception e) {
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Myron/Scripts/Services/MockMailService.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace Myron.Services {
+	public class MockMailService : IMailService {
+		public bool SendEmail(string from, string to, string subject, string body)
+		{
+			Debug.WriteLine($"SendMail: {subject}");
+			return true;
+		}
+	}
+}
